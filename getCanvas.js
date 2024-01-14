7b38f54a35f417051f1190f47826dd4f62355277
Veneiro <50624734+Veneiro@users.noreply.github.com>
2024-01-14 18:17:26 +0100

    limpieza del getCanvas.js
    Eliminación de funciones y variables que no se usaban para el funcionamiento de la aplicación

--- getCanvas.js
+++ getCanvas.js
@@ -1,44 +1,8 @@
 let canvasElement;
 let canvasCtx;
-let xHandLeft;
-let yHandLeft;
-let xHandRight;
-let yHandRight;
-let x1Right;
-let x1Left;
-let x2Right;
-let x2Left;
-let x3Right;
-let x3Left;
 
-
+// Guarda el canvas de la app para posteriormente dibujar en él
 function getP5Canvas(){
     canvasElement = document.getElementById("p5canvas");
     canvasCtx = canvasElement.getContext("2d");
-}
-
-function setCoordsLeft(x, y){
-    this.xHandLeft = x;
-    this.yHandLeft = y;
-}
-
-function setCoordsRight(x, y){
-    this.xHandRight = x;
-    this.yHandRight = y;
-}
-
-function getXHandLeft(){
-    return this.xHandLeft;
-}
-
-function getYHandLeft(){
-    return this.yHandLeft;
-}
-
-function getXHandRight(){
-    return this.xHandRight;
-}
-
-function getYHandRight(){
-    return this.yHandRight;
 }
